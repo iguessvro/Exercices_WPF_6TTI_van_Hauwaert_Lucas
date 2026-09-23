5aee60f960725b39d82feefc99fc883b5d615303
iguessvro <250594@site.asty-moulin.be>
2026-09-23 11:30:01 +0200

    Exercices mixtes de l'act 1* (et non pas act 0)

---
+++ 6TTI_van_Hauwaert_Lucas_ACT0_WPF_Ex_Mix/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _6TTI_van_Hauwaert_Lucas_ACT0_WPF_Ex3
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
